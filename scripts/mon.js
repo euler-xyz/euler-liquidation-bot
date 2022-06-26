1237bfa1a0183be9f6a7a185950a8efbd73ced4a
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-27 02:03:45 +0800

    Delete scripts directory

--- scripts/mon.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-
-const { main } = require('./monLib');
-
-main();
--- scripts/strategies/index.js
+++
@@ -1,7 +0,0 @@
-// const BotSwapAndRepay = require('./BotSwapAndRepay');
-const EOASwapAndRepay = require('./EOASwapAndRepay');
-
-module.exports = {
-  // BotSwapAndRepay,
-  EOASwapAndRepay,
-};
